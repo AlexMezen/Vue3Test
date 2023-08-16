7448274b98cd5399bc26356707d8f2867a978488
Alexander Mezentsev <mezentsev20001028@gmail.com>
2023-08-16 18:49:19 +0300

    Styles for select and input in edit has been done

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,47 +1,3 @@
-<!-- <template>
-<div class="post">
-    <div>
-    <div><strong>Post title:</strong> {{post.title}}</div>
-    <div><strong>Description:</strong> {{post.body}}</div>
-    
-    </div>
-    <div class="pst-btns">
-        <post-button @click="$emit('edit', post)">Edit</post-button>
-        <post-button @click="$emit('remove', post)">Delete</post-button>
-    </div>
-</div>
-</template>
-
-<script>
-import PostButton from './PostButton.vue';
-export default{
-    components:{
-        PostButton,
-    },
-    props:{
-        post:{
-            type:Object,
-            required:true,
-        }
-    },
-    
-}
-</script>
-
-<style scoped>
-.post{
-    padding: 15px;
-    margin: 15px;
-    border: 2px solid rgb(0, 92, 128);
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-}
-.pst-btns{
-    margin-top: -15px;
-    margin-right: -15px;
-}
-</style> -->
 
 <template>
     <div class="post">
@@ -50,14 +6,14 @@ export default{
         <div><strong>Description:</strong> {{ post.body }}</div>
       </div>
       <div v-else>
-        <post-input v-model="editedTitle" placeholder="Title"/>
-        <post-input v-model="editedBody" placeholder="Description"/>
+        <post-input class="inpList" v-focus v-model="editedTitle" placeholder="Title"/>
+        <post-input class="inpList" v-model="editedBody" placeholder="Description"/>
       </div>
       
         <div class="pst-btns">
-        <post-button @click="toggleEditMode">{{ editing ? 'Save' : 'Edit' }}</post-button>
+        <post-button :class="{ 'btn-edit': !editing, 'btn-save': editing }" @click="toggleEditMode">{{ editing ? 'Save' : 'Edit' }}</post-button>
         <post-button class="btn-del" @click="$emit('remove')">Delete</post-button>
-        <post-button @click="toggleCheckmark" :class="{ 'checked': isChecked }">Done</post-button>
+        <!-- <post-button @click="toggleCheckmark" :class="{ 'checked': isChecked }">Done</post-button> -->
       </div>
     </div>
   </template>
@@ -103,26 +59,13 @@ export default{
   };
   </script>
   <style scoped>
-  /* .post{
-      padding: 15px;
-      margin: 15px;
-      border: 2px solid rgb(0, 92, 128);
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-  }
-  .pst-btns{
-      margin-top: -15px;
-      margin-right: -20px;
-      width: 15%;
-    } */
     .btn-del{
       margin-right: 15px;
     }
-    .checked::after {
+    /* .checked::after {
       content: "\2713"; 
       margin-right: 1px;      
-    }
+    } */
   .post {
   display: flex;
   justify-content: space-between;
@@ -142,6 +85,16 @@ export default{
     margin-left: 10px;
     width: 68px;
 }
+.inpList{
+  padding: 0px;
+  width: 250px;
+  margin-right: 20px;
+}
+.btn-save {
+  background-color: teal; 
+  color: aliceblue;
+}
+
 
   </style>
   
--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -1,104 +1,117 @@
 <template>
-    <div>
-        <h1 class="pwp">Page with posts</h1>
-        <div class="inpcl"><post-input v-focus v-model="searchQuery" placeholder="Search..."/></div>
-      <div class="mtb"><post-button @click="showDialog">Create a post</post-button></div>  
-      <div class="mtb"><post-select v-model="selectedSort" :options="sortOptions"></post-select></div>
-        <dialog-post v-model:show="dialogVisible">
-        <post-form @create="createPost"></post-form>
-        </dialog-post>
-       <post-list :posts="SortedSearchedPosts" @remove="removePost" @edit="editPost" v-if="!postsLoading"></post-list>
-       <h3 v-else>Loading...</h3>
-       <div ref ='observer' class="observer"></div>
-       
+  <div>
+    <h1 class="pwp">Page with posts</h1>
+    <div class="inpcl">
+      <post-input v-focus v-model="searchQuery" placeholder="Search..." />
     </div>
-
+    <div class="mtb">
+      <post-button @click="showDialog">Create a post</post-button>
+    </div>
+    <div class="mtbSelect">
+      <post-select v-model="selectedSort" :options="sortOptions"></post-select>
+    </div>
+    <dialog-post v-model:show="dialogVisible">
+      <post-form @create="createPost"></post-form>
+    </dialog-post>
+    <post-list
+      :posts="SortedSearchedPosts"
+      @remove="removePost"
+      @edit="editPost"
+      v-if="!postsLoading"
+    ></post-list>
+    <h3 v-else>Loading...</h3>
+    <div ref="observer" class="observer"></div>
+  </div>
 </template>
 <script>
-import PostForm from "@/components/PostForm"
-import PostList from "@/components/PostList"
-import DialogPost from "@/components/DialogPost.vue"
-import PostButton from "@/components/PostButton.vue"
-import PostSelect from "@/components/PostSelect.vue"
-import PostInput from "@/components/PostInput.vue"
-import axios from "axios"
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+import DialogPost from "@/components/DialogPost.vue";
+import PostButton from "@/components/PostButton.vue";
+import PostSelect from "@/components/PostSelect.vue";
+import PostInput from "@/components/PostInput.vue";
+import axios from "axios";
 
-export default{
-    components: {
-        PostForm, PostList, DialogPost, PostButton, PostSelect, PostInput,
+export default {
+  components: {
+    PostForm,
+    PostList,
+    DialogPost,
+    PostButton,
+    PostSelect,
+    PostInput,
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false,
+      postsLoading: false,
+      selectedSort: "",
+      searchQuery: "",
+      page: 1,
+      limit: 10,
+      totalPages: 0,
+      sortOptions: [
+        { value: "title", name: "By name" },
+        { value: "body", name: "By content" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+      this.dialogVisible = false;
+      // this.savePostsToLocalStorage();
     },
-    data(){
-        return{
-            posts: [],
-            dialogVisible: false,
-            postsLoading: false,
-            selectedSort: '',
-            searchQuery: '',
-            page: 1,
-            limit: 10,
-            totalPages: 0,
-            sortOptions:[
-                {value: 'title', name: 'By name'},
-                {value: 'body', name: 'By content'}
-            ] 
-        }
-        
+    removePost(post) {
+      this.posts = this.posts.filter((p) => p.id !== post.id);
+      // this.savePostsToLocalStorage();
     },
-    methods:{
-        
-        createPost(post){
-            this.posts.push(post)
-            this.dialogVisible = false;
-            this.savePostsToLocalStorage();
-        },
-        removePost(post){
-            this.posts = this.posts.filter(p => p.id !== post.id)
-            this.savePostsToLocalStorage();
-
-        },
-        editPost(editedPost) {
-
-
-      const index = this.posts.findIndex(post => post.id === editedPost.id);
+    editPost(editedPost) {
+      const index = this.posts.findIndex((post) => post.id === editedPost.id);
       if (index !== -1) {
         const updatedPosts = [...this.posts];
         updatedPosts[index] = editedPost;
         this.posts = updatedPosts;
       }
-      this.savePostsToLocalStorage();
-
+      // this.savePostsToLocalStorage();
     },
     
-        showDialog(){
-            this.dialogVisible = true;
-        },
-        async fetchPosts() {
-      try {
-        this.postsLoading = true;
 
-        const localPosts = JSON.parse(localStorage.getItem('posts'));
-        if (localPosts) {
-          this.posts = localPosts;
-          this.postsLoading = false;
-        } else {
-          const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-            params: {
-              _page: this.page,
-              _limit: this.limit
-            }
-          });
-          this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
-          this.posts = response.data;
-          this.postsLoading = false;
-          this.savePostsToLocalStorage();
-        }
-      } catch (e) {
-        alert('Error');
-      }
+    showDialog() {
+      this.dialogVisible = true;
     },
-        async loadMorePosts(){
+    // async fetchPosts() {
+    //   try {
+    //     this.postsLoading = true;
+
+    //     const localPosts = JSON.parse(localStorage.getItem("posts"));
+    //     if (localPosts) {
+    //       this.posts = localPosts;
+    //       this.postsLoading = false;
+    //     } else {
+    //       const response = await axios.get(
+    //         "https://jsonplaceholder.typicode.com/posts",
+    //         {
+    //           params: {
+    //             _page: this.page,
+    //             _limit: this.limit,
+    //           },
+    //         }
+    //       );
+    //       this.totalPages = Math.ceil(
+    //         response.headers["x-total-count"] / this.limit
+    //       );
+    //       this.posts = response.data;
+    //       this.postsLoading = false;
+    //     }
+    //   } catch (e) {
+    //     alert("Error");
+    //   }
+    // },
+    async fetchPosts(){
             try{
-                this.page += 1; 
+                this.postsLoading = true;
                 let response = await axios.get('https://jsonplaceholder.typicode.com/posts',{
                     params:{
                         _page: this.page,
@@ -106,67 +119,95 @@ export default{
                     } 
                 })
                 this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
-                this.posts = [...this.posts, ...response.data];
+                this.posts = response.data;
+                this.postsLoading = false;
             } 
             catch(e){
                 alert('Error')
-            }
-        },
-        savePostsToLocalStorage() {
-      localStorage.setItem('posts', JSON.stringify(this.posts));
-    }
+            }},
+    async loadMorePosts() {
+      try {
+        this.page += 1;
+        let response = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts",
+          {
+            params: {
+              _page: this.page,
+              _limit: this.limit,
+            },
+          }
+        );
+        this.totalPages = Math.ceil(
+          response.headers["x-total-count"] / this.limit
+        );
+        this.posts = [...this.posts, ...response.data];
+      } catch (e) {
+        alert("Error");
+      }
     },
-    mounted(){
-        this.fetchPosts();
-        const options = {
-  rootMargin: "0px",
-  threshold: 1.0,
-};
-const callback =  (entries, observer) => {
-    if(entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts();
-    }
-};
-const observer = new IntersectionObserver(callback, options);
-observer.observe(this.$refs.observer)
-    },
-    computed: {
-        sortedPosts(){
-            return [...this.posts].sort((post1,post2)=>post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
-        },
-        SortedSearchedPosts(){
-        return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery))
-    }
-    },
-    created() {
-    const localPosts = JSON.parse(localStorage.getItem('posts'));
-    if (localPosts) {
-      this.posts = localPosts;
-    }
+    // savePostsToLocalStorage() {
+    //   localStorage.setItem("posts", JSON.stringify(this.posts));
+    // },
   },
-}
+  mounted() {
+    this.fetchPosts();
+    const options = {
+      rootMargin: "0px",
+      threshold: 1.0,
+    };
+    const callback = (entries, observer) => {
+      if (entries[0].isIntersecting && this.page < this.totalPages) {
+        this.loadMorePosts();
+      }
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(this.$refs.observer);
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) =>
+        post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      );
+    },
+    SortedSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.toLowerCase().includes(this.searchQuery)
+      );
+    },
+  },
+  // created() {
+  //   const localPosts = JSON.parse(localStorage.getItem("posts"));
+  //   if (localPosts) {
+  //     this.posts = localPosts;
+  //   }
+  // },
+};
 </script>
 <style>
-
-.mtb{
-    display: inline-block;   
-    margin-top: 10px;
-    margin-bottom: 20px;
-    margin-left: 15px;
-    
+.mtb {
+  display: inline-block;
+  margin-top: 20px;
+  margin-bottom: 15px;
+  margin-left: 15px;
 }
-.pwp{
-    margin-left: 15px;
+.mtbSelect {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: flex-end;
+  margin: 15px;
 }
-.inpcl{
-    width: 20%;
-    margin-left: 15px;
+.pwp {
+  margin-left: 15px;
 }
-::selection{
-    color: whitesmoke;
-    background-color: darkcyan;
+.inpcl {
+  width: 20%;
+  margin-left: 15px;
 }
-.observer{
-    height: 30px;
+::selection {
+  color: whitesmoke;
+  background-color: darkcyan;
+}
+.observer {
+  height: 30px;
 }
 </style>
