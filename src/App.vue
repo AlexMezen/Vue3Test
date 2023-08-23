52856391af3cfae86afc9a0cfa582b3eabd2908b
Alexander Mezentsev <mezentsev20001028@gmail.com>
2023-08-23 15:16:57 +0300

    page transition animation was done

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-<nav-bar></nav-bar>
-<div class="app"></div>
-<router-view></router-view>
-</template>
+    <nav-bar></nav-bar>
+    <div class="app">
+      <transition name="page" mode="out-in">
+        <router-view :key="$route.fullPath"></router-view>
+      </transition>
+    </div>
+  </template>
+  
 
 <script>
 import NavBar from '@/components/NavBar.vue'
@@ -22,5 +26,12 @@ export default{
 }
 .app{
     padding: 20px;
+}
+.page-enter-active,
+.page-leave-active {
+    transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to {
+  opacity: 0;
 }
 </style>
